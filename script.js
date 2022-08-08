7a3e34b8824c84ff18f6dabed1c25371b253e172
Nori <nori.terada9@gmail.com>
2022-08-08 03:46:07 -0600

    added simplye styling to the webpage. basic code for the javascript.

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(){
+    this.author = author
+    this.title = title
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary(){
+    myLibray.push(new Book(author,title,pages,read)) 
+}
