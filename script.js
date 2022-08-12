0a7423649515a75f1cf9863dfde6178fb96709b7
Nori <nori.terada9@gmail.com>
2022-08-11 18:29:27 -0600

    Added add book button function for it to open and close

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+const openPopupButtons = document.querySelectorAll("[data-popup-target]")
+const overlay = document.getElementById('overlay')
+
+openPopupButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        const popup = document.querySelector(button.dataset.popupTarget)
+        openPopup(popup)
+    })
+})
+function openPopup(popup){
+    if (popup == null) return
+    popup.classList.add('active')
+    overlay.classList.add("active") 
+}
+
+overlay.addEventListener("click",()=>{
+    const popups = document.querySelectorAll('.popup.active')
+    popups.forEach(popup => {
+        closePopup(popup)
+    }) 
+})
+
+function closePopup(popup){
+    if (popup == null) return
+    popup.classList.remove('active')
+    overlay.classList.remove("active") 
+}
+
+
+
+
+
+
+
 let myLibrary = [];
 
 function Book(){
